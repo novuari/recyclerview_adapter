074283e1ebe258460ab2773b122a37b62a1a4b7f
Ari Valencia <ari.valencia@novucentral.com>
2021-12-20 15:56:59 -0600

    first test successful

--- app/src/main/java/com/novu/adapterexample/MainActivity.kt
+++ app/src/main/java/com/novu/adapterexample/MainActivity.kt
@@ -2,10 +2,19 @@ package com.novu.adapterexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.novu.adapter.GAdapter
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rv = findViewById<RecyclerView>(R.id.list)
+
+        val adapter = GAdapter<String>(R.layout.item_string, BR.str_s)
+        adapter.setList(listOf("one", "two", "three", "four", "five"))
+        rv.adapter = adapter
     }
 }
